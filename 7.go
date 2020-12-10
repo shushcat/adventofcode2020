09288d6cddfce25f927e078798af8bf4b0e6bc20
J. O. Brickley <me@jobrickley.com>
2020-12-10 11:12:03 -0800

    Merge 7s & begin refactor of part 1

--- 7.go
+++ 7.go
@@ -8,11 +8,49 @@ import (
 	"strings"
 )
 
-// type rule map[string]map[string]int
-
 type rule struct {
 	name string
 	held map[string]int
+}
+
+// func (bag rule) containedByV1(rs []rule) int {
+// 	canContain := 0
+// 	for i, _ := range rs {
+// 		_, contains := searchContents(rs[i], rs, bag.name, 1, false)
+// 		if contains {
+// 			canContain += 1
+// 		}
+// 	}
+// 	return canContain
+// }
+
+func (r rule) containedBy(rs []rule) int {
+	cb := 0
+	for _, v := range rs  {
+		for k, _ := range v.held {
+			fmt.Println(k,v)
+			if k == r.name {
+				return 1
+			} else if k == "none" {
+				return 0
+			}
+			// if i, ok := keyIndex(v.name, rs); ok {
+				// cb += (r.containedBy(rs[i:]))
+			// }
+		}
+	}
+	return cb
+}
+
+func (r rule) numHeld(rs []rule) int {
+	nh := 0
+	for k, _ := range r.held {
+		nh += r.held[k]
+		if i, ok := keyIndex(k, rs); ok {
+			nh += (rs[i].numHeld(rs) * r.held[k])
+		}
+	}
+	return nh
 }
 
 func parseRule(line string) rule {
@@ -35,13 +73,6 @@ func parseRule(line string) rule {
 	return r
 }
 
-func ruleContains(r rule, bag string) bool {
-	if _, ok := r.held[bag]; ok {
-		return true
-	}
-	return false
-}
-
 func readRules(path string) []rule {
 	file, _ := os.Open(path)
 	defer file.Close()
@@ -53,16 +84,6 @@ func readRules(path string) []rule {
 	return rs
 }
 
-func delRule(rs []rule, r rule) []rule {
-	rs2 := []rule{}
-	for _, v := range rs {
-		if v.name != r.name {
-			rs2 = append(rs2, v)
-		}
-	}
-	return rs2
-}
-
 func keyIndex(key string, rs []rule) (int, bool) {
 	for i, v := range rs {
 		if v.name == key {
@@ -72,14 +93,21 @@ func keyIndex(key string, rs []rule) (int, bool) {
 	return 0, false
 }
 
-func min(ary []int) int {
-	m := ary[0]
-	for _, n := range ary {
-		if n < m {
-			m = n
+func ruleContains(r rule, bag string) bool {
+	if _, ok := r.held[bag]; ok {
+		return true
+	}
+	return false
+}
+
+func delRule(rs []rule, r rule) []rule {
+	rs2 := []rule{}
+	for _, v := range rs {
+		if v.name != r.name {
+			rs2 = append(rs2, v)
 		}
 	}
-	return m
+	return rs2
 }
 
 func searchContents(r rule, rs []rule, bag string, depth int, found bool) (int, bool) {
@@ -100,28 +128,12 @@ func searchContents(r rule, rs []rule, bag string, depth int, found bool) (int, 
 	return depth, found
 }
 
-func canContain(rs []rule, bag string) int {
-	canContain := 0
-	for i, _ := range rs {
-		_, contains := searchContents(rs[i], rs, bag, 1, false)
-		if contains {
-			canContain += 1
-		}
-	}
-	return canContain
-}
-
 func main() {
-	// path := "7_small.txt"
-	path := "7.txt"
+	// path := "7.txt"
+	path := "7_small.txt"
 	rs := readRules(path)
-	fmt.Println(canContain(rs, "shiny gold"))
-	// fmt.Println(searchContents(rs[0], rs, "muted yellow", 1, false))
-	// fmt.Println(searchContents(rs[0], rs, "vibrant plum", 1, false))
-	// fmt.Println(searchContents(rs[0], rs, "shiny gold", 1, false))
-	// fmt.Println(searchContents(rs[4], rs, "light red", 1, false))
-	// fmt.Println(containmentDistance(rs["muted yellow"], rs, "faded blue"))
-	// for _, r := range rs {
-	// fmt.Println("Does", r, "contain 'shiny gold'?", ruleContains(r, "shiny gold"))
-	// }
+	i, _ := keyIndex("shiny gold", rs)
+	fmt.Println("Part 1: ", rs[i].containedBy(rs))
+	// fmt.Println("Part 1: ", rs[i].containedByV1(rs))
+	fmt.Println("Part 2: ", rs[i].numHeld(rs))
 }
