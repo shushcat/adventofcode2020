69cfc234cd3fb7b07b4bdf88017b69ea13c7d422
J. O. Brickley <me@jobrickley.com>
2020-12-23 15:38:31 -0800

    Finish image assembly from tiles

--- 20.go
+++ 20.go
@@ -10,71 +10,29 @@ import (
 type tile struct {
 	x, y int
 	id   int
-	pat  [][]string
+	pat  [10][10]string
 }
+
 type stack map[int]tile
-
-func (t tile) Corner(ts stack) bool {
-	// compare 1951 and 2311 at first
-	fmt.Println("entered")
-	printTile(t)
-	matches := 0
-	// t2 := ts[2311]
-	for _, t2 := range ts {
-		if t3, ok, side := fitTile(t, t2); ok {
-			fmt.Println(t3.id, "matches on side", side)
-			matches += 1
-		}
-	}
-	fmt.Println(matches)
-	fmt.Println(len(ts))
-	// printTile(t2)
-	// }
-	// for id, t2 := range ts {
-	// }
-	// t2 = rotateTileRight(t2)
-	// printTile(ts[2311])
-	// fmt.Println()
-	// printTile(t2)
-	// fmt.Println()
-	return false
-}
-
-func findCorner(ts stack) tile {
-	matches := 0
-	corner := tile{0, 0, 0, [][]string{}}
-	fmt.Println(len(ts))
-	for _, t1 := range ts {
-		for _, t2 := range ts {
-			if _, ok, _ := fitTile(t1, t2); ok {
-				matches += 1
-			}
-		}
-		if matches == 2 {
-			corner = tile{t1.x, t1.y, t1.id, t1.pat}
-			break
-		}
-	}
-
-	// fmt.Println(matches)
-	// fmt.Println(len(ts))
-	fmt.Println(corner.id)
-	printTile(corner)
-	return corner
-}
 
 func readTiles(path string) stack {
 	b, _ := ioutil.ReadFile(path)
 	input := string(b)
-	t := tile{0, 0, 0, [][]string{}}
+	t := tile{}
 	ts := stack{}
+	i := 0
 	for _, l := range strings.Split(input, "\n") {
 		if strings.HasPrefix(l, "Tile") {
 			l = strings.TrimPrefix(l, "Tile ")
 			l = strings.TrimSuffix(l, ":")
 			t.id, _ = strconv.Atoi(l)
+			i = 0
 		} else if strings.HasPrefix(l, ".") || strings.HasPrefix(l, "#") {
-			t.pat = append(t.pat, strings.Split(l, ""))
+			cs := strings.Split(l, "")
+			for j := 0; j < 10; j++ {
+				t.pat[i][j] = cs[j]
+			}
+			i++
 		}
 		if l == "" {
 			ts[t.id] = t
@@ -84,29 +42,27 @@ func readTiles(path string) stack {
 	return ts
 }
 
-func printTile(t tile) {
-	for row := 0; row < len(t.pat); row++ {
-		fmt.Println(t.pat[row])
+func printAryAry(a [][]string) {
+	for row := 0; row < len(a); row++ {
+		fmt.Println(a[row])
 	}
 }
 
 func rotateTileRight(t tile) tile {
-	t2 := tile{t.x, t.y, t.id, [][]string{}}
+	t2 := tile{t.x, t.y, t.id, [10][10]string{}}
 	for row := 9; row >= 0; row-- {
-		t2.pat = append(t2.pat, []string{})
 		for col := 0; col < 10; col++ {
-			t2.pat[9-row] = append(t2.pat[9-row], t.pat[9-col][9-row])
+			t2.pat[9-row][col] = t.pat[9-col][9-row]
 		}
 	}
 	return t2
 }
 
 func flipTileRight(t tile) tile {
-	t2 := tile{t.x, t.y, t.id, [][]string{}}
+	t2 := tile{t.x, t.y, t.id, [10][10]string{}}
 	for row := 0; row < 10; row++ {
-		t2.pat = append(t2.pat, []string{})
 		for col := 9; col >= 0; col-- {
-			t2.pat[row] = append(t2.pat[row], t.pat[row][col])
+			t2.pat[row][9-col] = t.pat[row][col]
 		}
 	}
 	return t2
@@ -114,102 +70,211 @@ func flipTileRight(t tile) tile {
 
 // Returns a string holding the characters of a given edge, joined in order
 // from left to right and top to bottom.
-func getEdge(t tile, side int) string {
-	var edge []string
+func getSide(t tile, side int) [10]string {
+	var sideChars [10]string
 	switch side {
 	case 0:
-		edge = t.pat[0]
+		sideChars = t.pat[0]
 	case 1:
 		for i := 0; i < 10; i++ {
-			edge = append(edge, t.pat[i][9])
+			sideChars[i] = t.pat[i][9]
 		}
 	case 2:
-		edge = t.pat[9]
+		sideChars = t.pat[9]
 	case 3:
 		for i := 0; i < 10; i++ {
-			edge = append(edge, t.pat[i][0])
+			sideChars[i] = t.pat[i][0]
 		}
 	}
-	return strings.Join(edge, "")
+	return sideChars
 }
 
 // The `side` corresponds to the edges of t1 and falls in the range (0, 3).
 // For a given value of `side`, the return value is the result of an attempted
 // match with the opposite side of t2.
-func matchEdge(t1 tile, t2 tile, side int) bool {
-	var t1Edge, t2Edge string
+func matchSide(t1 tile, t2 tile, side int) bool {
+	var t1Side, t2Side [10]string
 	match := false
-	t1Edge, t2Edge = getEdge(t1, side), getEdge(t2, (side+2)%4)
-	match = t1Edge == t2Edge
+	t1Side, t2Side = getSide(t1, side), getSide(t2, (side+2)%4)
+	match = t1Side == t2Side
 	return match
 }
 
-func fitTile(t1 tile, t2 tile) (tile, bool, int) {
+func fitTile(t1 tile, t2 tile) (tile, bool) {
 	match := false
 	if t1.id == t2.id {
-		return t2, false, -1
+		return t2, false
 	}
 	for flip := 0; flip < 2; flip++ {
 		for rot := 0; rot < 4; rot++ {
-			for edge := 0; edge < 4; edge++ {
-				match = matchEdge(t1, t2, edge)
+			for side := 0; side < 4; side++ {
+				match = matchSide(t1, t2, side)
 				if match {
-					switch edge {
+					switch side {
 					case 0:
-						t2.x, t2.y = t1.x, t1.y + 1
+						t2.x, t2.y = t1.x, t1.y+1
 					case 1:
-						t2.x, t2.y = t1.x + 1, t1.y
+						t2.x, t2.y = t1.x+1, t1.y
 					case 2:
-						t2.x, t2.y = t1.x, t1.y - 1
+						t2.x, t2.y = t1.x, t1.y-1
 					case 3:
-						t2.x, t2.y = t1.x - 1, t1.y + 1
+						t2.x, t2.y = t1.x-1, t1.y
 					}
-					return t2, true, edge
+					return t2, true
 				}
 			}
 			t2 = rotateTileRight(t2)
 		}
 		t2 = flipTileRight(t2)
 	}
-	return t2, false, -1
+	return t2, false
 }
 
-func cornerProduct(ts stack) int {
-	prod := 1
-	disqualified := map[int]bool{}
-	for _, t1 := range ts {
-		if !disqualified[t1.id] {
-			matches := 0
-			for _, t2 := range ts {
-				if _, ok, _ := fitTile(t1, t2); ok {
-					matches += 1
-				}
-			}
-			if matches == 2 {
-				prod = prod * t1.id
-			} else {
-				disqualified[t1.id] = true
-			}
-		}
-	}
+func cornerProduct(layout [][]int) int {
+	nw := layout[0][0]
+	ne := layout[0][len(layout)-1]
+	sw := layout[len(layout)-1][0]
+	se := layout[len(layout)-1][len(layout)-1]
+	prod := nw * ne * sw * se
 	return prod
 }
 
+func tileChain(t1 tile, chained stack, ts stack) stack {
+	chained[t1.id] = t1
+	if len(chained) != len(ts) {
+		for _, t2 := range ts {
+			if t3, ok := fitTile(t1, t2); ok {
+				t1 = t3
+				chained[t1.id] = t1
+				break
+			}
+		}
+		chained = tileChain(t1, chained, ts)
+	}
+	return chained
+}
+
+func seedTile(ts stack) tile {
+	t1 := tile{}
+	for _, t2 := range ts {
+		t1 = t2
+		break
+	}
+	return t1
+}
+
+func normalizeTileCoordinates(ts stack) (stack, int, int) {
+	xMin, yMin := 0, 0
+	for _, t := range ts {
+		if t.x < xMin {
+			xMin = t.x
+		}
+		if t.y < yMin {
+			yMin = t.y
+		}
+	}
+	xNorm, yNorm := -xMin, -yMin
+	normStack := stack{}
+	for k, t := range ts {
+		normStack[k] = tile{t.x + xNorm, t.y + yNorm, t.id, t.pat}
+	}
+	xMax, yMax := 0, 0
+	for _, t := range normStack {
+		if t.x > xMax {
+			xMax = t.x
+		}
+		if t.y > yMax {
+			yMax = t.y
+		}
+	}
+	return normStack, xMax, yMax
+}
+
+func layoutTiles(ts stack) [][]int {
+	tc := stack{}
+	seed := seedTile(ts)
+	tc = tileChain(seed, tc, ts)
+	normalized, xMax, yMax := normalizeTileCoordinates(tc)
+	layout := [][]int{}
+	for y := 0; y <= yMax; y++ {
+		layout = append(layout, []int{})
+		for x := 0; x <= xMax; x++ {
+			layout[y] = append(layout[y], 0)
+		}
+	}
+	for _, t := range normalized {
+		layout[t.y][t.x] = t.id
+	}
+	return layout
+}
+
+func joinTiles(ts stack, layout [][]int) [][]string {
+	image := [][]string{}
+	return image
+}
+
+func printLayout(layout [][]int) {
+	for i := 0; i < len(layout); i++ {
+		fmt.Println(layout[i])
+	}
+}
+
+func shrinkTile(t tile) [][]string {
+	t2 := [][]string{}
+	for row := 1; row < 9; row++ {
+		t2 = append(t2, []string{})
+		for col := 1; col < 9; col++ {
+			t2[row-1] = append(t2[row-1], t.pat[row][col])
+		}
+	}
+	return t2
+}
+
+// Accepts an image, a tile shrunk to 8 by 8, and the row and column of that
+// tile in the tile layout.
+func insertTile(img [][]string, st [][]string, lRow int, lCol int) [][]string {
+	for row := 0; row < 8; row++ {
+		for col := 0; col < 8; col++ {
+			iRow := row + (lRow * 8)
+			iCol := col + (lCol * 8)
+			img[iRow][iCol] = st[row][col]
+		}
+	}
+	return img
+}
+
+func assembleImage(layout [][]int, ts stack) [][]string {
+	layoutSideLen := len(layout)
+	imgSideLen := layoutSideLen * 8
+	img := [][]string{}
+	// Initialize image.
+	for row := 0; row < imgSideLen; row++ {
+		img = append(img, []string{})
+		for col := 0; col < imgSideLen; col++ {
+			img[row] = append(img[row], "x")
+		}
+	}
+	// Place shrunken tiles in the image.
+	for row := 0; row < layoutSideLen; row++ {
+		for col := 0; col < layoutSideLen; col++ {
+			tileID := layout[row][col]
+			insertTile(img, shrinkTile(ts[tileID]), row, col)
+		}
+	}
+	return img
+}
+
+func patAtPoint() {
+	panic // TODO Here's where it goes.
+}
+
 func main() {
-	fmt.Println("======TEST======")
+	fmt.Println("============TEST============")
 	path := "20_small.txt"
 	// path := "20.txt"
 	ts := readTiles(path)
-	// for _, t := range ts {
-		// fmt.Println(t.id, t.x, t.y)
-	// }
-	findCorner(ts)
-	// matched := 0
-	// fmt.Println(matched)
-	// ts[1951].Corner(ts)
-	// ts[2311].Corner(ts)
-	// fmt.Println(Part 1: cornerProduct(ts))
-	// fitTile(ts[1951], ts[2311])
-	// fitTile(ts[1951], ts[2729])
-	// ts[1951].Corner(ts)
+	// fmt.Println(ts)
+	layout := layoutTiles(ts)
+	assembleImage(layout, ts)
+	// fmt.Println("Part 1:", cornerProduct(layout))
 }
