bbb4ab833e99032534fead7c4e5a426e0f12f80f
J. O. Brickley <me@jobrickley.com>
2020-12-27 08:32:47 -0800

    Implement map[int]int; begin array version

--- 23.go
+++ 23.go
@@ -1,65 +1,78 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"os"
 	"log"
+	"os"
+	"runtime/pprof"
 	"strconv"
 	"strings"
-	"runtime/pprof"
-	"flag"
 )
 
-func populate(s string) map[int]int{
+type cups struct {
+	h, t int
+	m    map[int]int
+}
+
+func populate(s string) *cups {
 	ia := strings.Split(s, "")
-	cs := make(map[int]int, len(ia))
-	for i := 0; i< len(ia)-1;i++{
+	cs := &cups{}
+	cs.m = make(map[int]int, len(ia))
+	for i := 0; i < len(ia)-1; i++ {
 		d1, _ := strconv.Atoi(ia[i])
 		d2, _ := strconv.Atoi(ia[i+1])
-		cs[d1]=d2
+		cs.m[d1] = d2
 	}
+	cs.setHead()
+	cs.setTail()
 	return cs
 }
 
-func bigPopulate(s string) map[int]int {
+func bigPopulate(s string) *cups {
 	ia := strings.Split(s, "")
-	cs := make(map[int]int, len(ia))
-	for i := 0; i< len(ia)-1;i++{
+	cs := &cups{}
+	cs.h, cs.t = 0, 0
+	cs.m = make(map[int]int, len(ia))
+	for i := 0; i < len(ia)-1; i++ {
 		d1, _ := strconv.Atoi(ia[i])
 		d2, _ := strconv.Atoi(ia[i+1])
-		cs[d1]=d2
+		cs.m[d1] = d2
 	}
-	for i:=10; i<=1000000;i++{
-		cs[i]=i
+	for i := 10; i <= 1000000; i++ {
+		cs.m[i] = i
 	}
+	cs.setHead()
+	cs.setTail()
 	return cs
 }
 
-func step(cs map[int]int) map[int]int {
-	head, tail := head(cs), tail(cs)
+func (cs *cups) step() {
+	head, tail := cs.h, cs.t
 	cur := head
-	des := setDes(cur)
-	hBeg, hEnd := cs[cur], cs[cs[cs[cur]]]
-	cur = cs[hEnd]
+	des := head - 1
+	hBeg, hEnd := cs.m[cur], cs.m[cs.m[cs.m[cur]]]
+	cur = cs.m[hEnd]
+	cs.h = cs.m[hEnd]
 	for {
 		if cur == des {
 			if cur != tail {
-				cs[hEnd] = cs[des]
-				cs[tail] = head
+				cs.m[hEnd] = cs.m[des]
+				cs.m[cs.t] = head
 			} else {
-				cs[hEnd] = head
+				cs.m[hEnd] = head
 			}
-			cs[des] = hBeg
-			delete(cs, head)
+			cs.t = head
+			cs.m[des] = hBeg
+			delete(cs.m, head)
 			break
 		} else if cur == tail {
-			cur = cs[hEnd]
+			cur = cs.m[hEnd]
 			des = setDes(des)
 		} else {
-			cur = cs[cur]
+			cur = cs.m[cur]
 		}
 	}
-	return cs
 }
 
 func setDes(n int) int {
@@ -67,53 +80,48 @@ func setDes(n int) int {
 	if n > 1 {
 		des = n - 1
 	} else {
-		des = 9
+		des = 1000000
 	}
 	return des
 }
 
-func nSteps(cs map[int]int, n int) {
+func (cs *cups) nSteps(n int) {
 	for i := 0; i < n; i++ {
 		fmt.Println("Step", i+1)
-		cs = step(cs)
+		cs.step()
 	}
 }
 
-func head(cs map[int]int) int {
+func (cs *cups) setHead() int {
 	refd := make(map[int]bool)
-	head := 0
-	for _, v := range cs {
-		refd[v] = true
+	for _, v := range cs.m {
+		if _, ok := cs.m[v]; ok {
+			refd[v] = true
+		}
 	}
-	for k, _ := range cs {
+	for k, _ := range cs.m {
 		if !(refd[k]) {
-			head = k
+			cs.h = k
 			break
 		}
 	}
-	return head
+	return cs.h
 }
 
-func tail(cs map[int]int) int {
-	refd := make(map[int]bool)
-	tail := 0
-	for k, _ := range cs {
-		refd[k] = true
-	}
-	for _, v := range cs {
-		if !(refd[v]) {
-			tail = v
+func (cs *cups) setTail() {
+	for _, v := range cs.m {
+		if _, ok := cs.m[v]; !ok {
+			cs.t = v
 			break
 		}
 	}
-	return tail
 }
 
-func printCups(cs map[int]int) {
-	cur := head(cs)
-	for i:=0;i<=len(cs);i++ {
+func printCups(cs *cups) {
+	cur := cs.h
+	for i := 0; i <= len(cs.m); i++ {
 		fmt.Print(cur)
-		cur = cs[cur]
+		cur = cs.m[cur]
 	}
 	fmt.Print("\n")
 }
@@ -127,35 +135,15 @@ func has(a []int, n int) bool {
 	return false
 }
 
-func max(a []int) int {
-	m := 0
-	for i := 0; i < len(a); i++ {
-		if a[i] > m {
-			m = a[i]
-		}
-	}
-	return m
-}
-
-func min(a []int) int {
-	m := a[0]
-	for i := 0; i < len(a); i++ {
-		if a[i] < m {
-			m = a[i]
-		}
-	}
-	return m
-}
-
-func p1Str(cs map[int]int) string {
+func p1Str(cs *cups) string {
 	var str string
-	cur, tail := head(cs), tail(cs)
+	cur := cs.h
 	for {
 		str = str + strconv.FormatInt(int64(cur), 10)
-		if cur == tail {
+		if cur == cs.t {
 			break
 		}
-		cur = cs[cur]
+		cur = cs.m[cur]
 	}
 	a := strings.Split(str, "1")
 	str = a[1] + a[0]
@@ -174,14 +162,18 @@ func main() {
 		pprof.StartCPUProfile(f)
 		defer pprof.StopCPUProfile()
 	}
-	input := "389125467" // Sample
+	// input := "389125467" // Sample
 	// input := "289154673" // Sample
 	// input := "837419265" // Sample
-	// input := "398254716" // Personal
+	input := "398254716" // Personal
 	cs1 := populate(input)
-	nSteps(cs1, 100)
-	fmt.Println("Part 1:", p1Str(cs1))	// -> 45798623
+	cs1.nSteps(100)
+	fmt.Println("Part 1:", p1Str(cs1))
 	cs2 := bigPopulate(input)
-	nSteps(cs2, 10)
-	fmt.Println(cs2[1])
+	cs2.nSteps(100)
+	// fmt.Println(cs2.m[1])
+	// printCups(cs1)
+	// fmt.Println(cs2, cs2.head(), cs2.tail())
+	// nSteps(cs2, 10)
+	// fmt.Println(cs2[1])
 }
